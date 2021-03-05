3d9f068b885f41d2259ed4c4f000ee0c9b9c8310
Francesca Marinuzzi <france.marinuzzi0203@gmail.com>
2021-03-05 07:38:00 -0400

    firebase config 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-  apiKey: "AIzaSyCAkv4SZn49Xc7tV2LBs9Gr5ytjEYH7Buk",
-  authDomain: "microproyecto2-435e2.firebaseapp.com",
-  projectId: "microproyecto2-435e2",
-  storageBucket: "microproyecto2-435e2.appspot.com",
-  messagingSenderId: "107430987885",
-  appId: "1:107430987885:web:fc751a47f1d65ac3f99764",
-  measurementId: "G-B32S8NPYJ6"
+  apiKey: "AIzaSyDTRvSswshl-GZdIabXonrWRpXQvJoQMrI",
+  authDomain: "microproyecto2-67795.firebaseapp.com",
+  projectId: "microproyecto2-67795",
+  storageBucket: "microproyecto2-67795.appspot.com",
+  messagingSenderId: "760554385867",
+  appId: "1:760554385867:web:d30cb341663b278652464e",
+  measurementId: "G-R8PX7VB0FF"
   },
 };
